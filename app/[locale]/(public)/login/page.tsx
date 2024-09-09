01a3497d9e986efe18d23413bf38de4891f60ac8
tycebrown <91034018+tycebrown@users.noreply.github.com>
2024-09-08 20:17:34 -0500

    remove useless login changes

--- app/[locale]/(public)/login/page.tsx
+++ app/[locale]/(public)/login/page.tsx
@@ -2,70 +2,63 @@ import ModalView, { ModalViewTitle } from "@/app/components/ModalView";
 import Button from "@/app/components/Button";
 import FormLabel from "@/app/components/FormLabel";
 import TextInput from "@/app/components/TextInput";
-import LoginForm from "./LoginForm";
-import FieldError from "@/app/components/FieldError";
-import { verifySession } from "@/app/session";
-import { redirect, RedirectType } from "next/navigation";
-import { getLocale, getTranslations } from "next-intl/server";
+import LoginForm from './LoginForm'
+import FieldError from '@/app/components/FieldError';
+import { verifySession } from '@/app/session';
+import { redirect, RedirectType } from 'next/navigation';
+import { getLocale, getTranslations } from 'next-intl/server';
 import { Metadata, ResolvingMetadata } from "next";
 
-export async function generateMetadata(
-  _: any,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const t = await getTranslations("LoginPage");
-  const { title } = await parent;
+export async function generateMetadata(_: any, parent: ResolvingMetadata): Promise<Metadata> {
+  const t = await getTranslations("LoginPage")
+  const { title } = await parent
 
   return {
-    title: `${t("title")} | ${title?.absolute}`,
-  };
+    title: `${t("title")} | ${title?.absolute}`
+  }
 }
 
 export default async function LoginPage() {
-  const t = await getTranslations("LoginPage");
-  const locale = await getLocale();
+    const t = await getTranslations('LoginPage');
+    const locale = await getLocale()
 
-  const session = await verifySession();
-  if (session) {
-    redirect(`/${locale}/interlinear`, RedirectType.replace);
-  }
+    const session = await verifySession();
+    if (session) {
+        redirect(`/${locale}/interlinear`, RedirectType.replace)
+    }
 
-  return (
-    <ModalView className="max-w-[480px] w-full">
-      <ModalViewTitle>{t("title")}</ModalViewTitle>
-      <LoginForm>
-        <div className="mb-4">
-          <FormLabel htmlFor="email">{t("form.email")}</FormLabel>
-          <TextInput
-            id="email"
-            name="email"
-            className="w-full"
-            autoComplete="username"
-            aria-describedby="email-error"
-          />
-          <FieldError id="email-error" name="email" />
-        </div>
-        <div className="mb-6">
-          <FormLabel htmlFor="password">{t("form.password")}</FormLabel>
-          <TextInput
-            id="password"
-            type="password"
-            name="password"
-            className="w-full"
-            autoComplete="current-password"
-            aria-describedby="password-error"
-          />
-          <FieldError id="password-error" name="password" />
-        </div>
-        <Button type="submit" className="w-full mb-2">
-          {t("form.submit")}
-        </Button>
-        <div className="text-center">
-          <Button variant="tertiary" href="/forgot-password">
-            {t("forgot_password")}
-          </Button>
-        </div>
-      </LoginForm>
+    return <ModalView className="max-w-[480px] w-full">
+        <ModalViewTitle>{t('title')}</ModalViewTitle>
+        <LoginForm>
+            <div className="mb-4">
+                <FormLabel htmlFor="email">{t('form.email')}</FormLabel>
+                <TextInput
+                    id="email"
+                    name="email"
+                    className="w-full"
+                    autoComplete="username"
+                    aria-describedby="email-error"
+                />
+                <FieldError id="email-error" name="email"/>
+            </div>
+            <div className="mb-6">
+                <FormLabel htmlFor="password">{t('form.password')}</FormLabel>
+                <TextInput
+                    id="password"
+                    type="password"
+                    name="password"
+                    className="w-full"
+                    autoComplete="current-password"
+                    aria-describedby="password-error"
+                />
+                <FieldError id="password-error" name="password"/>
+            </div>
+            <Button type="submit" className="w-full mb-2">{t('form.submit')}</Button>
+            <div className="text-center">
+                <Button variant="tertiary" href="/forgot-password">
+                    {t("forgot_password")}
+                </Button>
+            </div>
+        </LoginForm>
     </ModalView>
-  );
 }
