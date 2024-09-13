6d56a0ef1558878a3a40006ea152d5c6b541fd20
Adrian Rocke <me@adrianrocke.com>
2024-09-12 22:14:03 -0500

    feat: db logging

--- app/db.ts
+++ app/db.ts
@@ -7,9 +7,19 @@ if (!connectionString) {
 }
  
 const pool = new Pool({ connectionString })
- 
-export function query<T extends QueryResultRow>(text: string, params: any): Promise<QueryResult<T>> {
-  return pool.query<T>(text, params)
+
+async function _query<T extends QueryResultRow>(client: pg.Pool | pg.PoolClient, text: string, params: any): Promise<QueryResult<T>> {
+  const start = performance.now()
+  const result = await client.query<T>(text, params)
+  const duration = performance.now() - start
+  if (process.env.LOG_DB_QUERIES === 'true') {
+      console.log(`QUERY ${duration.toFixed(0)}ms ${text.replaceAll(/\s+/g, ' ').slice(0, 100)} params: ${JSON.stringify(params)}`)
+  }
+  return result
+}
+
+export async function query<T extends QueryResultRow>(text: string, params: any): Promise<QueryResult<T>> {
+  return _query(pool, text, params)
 }
 
 export async function transaction<T>(tx: (q: typeof query) => Promise<T>): Promise<T> {
@@ -17,7 +27,7 @@ export async function transaction<T>(tx: (q: typeof query) => Promise<T>): Promi
 
     try {
         await client.query('BEGIN')
-        const result = await tx(client.query.bind(client))
+        const result = await tx((text: string, params: any) => _query(client, text, params))
         await client.query('COMMIT')
         return result
     } catch (error) {
