286e1105db90ce227912fc09f3d9083beaf19f2d
Adrian Rocke <me@adrianrocke.com>
2024-10-02 10:31:27 -0500

    feat: show which verse is being read

--- app/[locale]/(authenticated)/read/[code]/ReadingClientState.tsx
+++ app/[locale]/(authenticated)/read/[code]/ReadingClientState.tsx
@@ -4,15 +4,18 @@ import { createContext, ReactNode, useContext, useState } from "react";
 
 interface ReadingClientState {
     textSize: number
+    audioVerse?: string
     setTextSize(textSize: number): void
+    setAudioVerse(verseId?: string): void
 }
 
 const ReadingClientStateContext = createContext<ReadingClientState | null>(null)
 
 export function ReadingClientStateProvider({ children }: { children: ReactNode }) {
     const [textSize, setTextSize] = useState(3)
+    const [audioVerse, setAudioVerse] = useState<string>()
 
-    return <ReadingClientStateContext.Provider value={{ textSize, setTextSize }}>
+    return <ReadingClientStateContext.Provider value={{ textSize, audioVerse, setTextSize, setAudioVerse }}>
         {children}
     </ReadingClientStateContext.Provider>
 }
