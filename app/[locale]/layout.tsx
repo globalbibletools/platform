5d155c1caac5078a64610f350d1a8351a0c6446b
Adrian Rocke <me@adrianrocke.com>
2024-10-26 18:08:58 -0500

    feat: add chat bubble

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import "../globals.css";
 import { headFontClass } from "../fonts";
 import languages from "../../languages.json";
 import { FlashProvider } from "../flash";
+import Script from "next/script";
  
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations("RootLayout")
@@ -48,6 +49,22 @@ export default function RootLayout({
                 {children}
             </FlashProvider>
         </NextIntlClientProvider>
+        {
+            process.env.NODE_ENV === 'production' &&
+                <Script id="show-banner">
+                  {`
+                    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                    (function(){
+                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                    s1.async=true;
+                    s1.src='https://embed.tawk.to/671d701f4304e3196ad8d17a/1ib5frv2r';
+                    s1.charset='UTF-8';
+                    s1.setAttribute('crossorigin','*');
+                    s0.parentNode.insertBefore(s1,s0);
+                    })();
+                  `}
+                </Script>
+        }
       </body>
     </html>
   );
