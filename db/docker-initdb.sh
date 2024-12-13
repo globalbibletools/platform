c911911f6b75a98977f980d77fcc748038831154
Adrian Rocke <me@adrianrocke.com>
2024-12-13 16:25:04 -0600

    build: restart the database after setting up pg_cron

--- db/docker-initdb.sh
+++ db/docker-initdb.sh
@@ -2,5 +2,8 @@
 
 echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
 
+# Required to load pg_cron
+pg_ctl restart
+
 psql --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" -f /db/schema.sql;
 pg_restore -Fc --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" /db/data.dump;
