811b382791d32e582841e27446edca840633bef8
Adrian Rocke <me@adrianrocke.com>
2024-12-23 17:35:29 -0600

    build: move server to package

--- .workers/workers/worker-env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const env_1 = require("@next/env");
-const projectDir = process.cwd();
-(0, env_1.loadEnvConfig)(projectDir);
