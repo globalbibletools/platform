747ec97fcbc9f5a83265f20903e3418767c7600d
Adrian Rocke <me@adrianrocke.com>
2024-12-23 15:20:41 -0600

    refactor: update table/column names in weekly stats

--- db/migrations/24-12-22-standardize-conventions.sql
+++ db/migrations/24-12-22-standardize-conventions.sql
@@ -297,4 +297,47 @@ END;
 $$
 LANGUAGE 'plpgsql';
 
+CREATE OR REPLACE FUNCTION public.generate_gloss_statistics_for_week(d TIMESTAMP)
+RETURNS void
+LANGUAGE SQL
+AS $$
+    INSERT INTO weekly_gloss_statistics (week, language_id, book_id, user_id, approved_count, unapproved_count)
+    SELECT
+        (DATE_BIN('7 days', DATE_TRUNC('day', $1), TIMESTAMP '2024-12-15')),
+        log.language_id, log.book_id, log.updated_by,
+        COUNT(*) FILTER (WHERE log.state = 'APPROVED'),
+        COUNT(*) FILTER (WHERE log.state = 'UNAPPROVED')
+    FROM (
+        SELECT
+            DISTINCT ON (log.phrase_id, phrase_word.word_id, verse.book_id)
+            log.updated_by,
+            log.state,
+            phrase.language_id,
+            verse.book_id
+        FROM (
+            (
+                SELECT phrase_id, updated_by, updated_at, gloss, state
+                FROM gloss
+            ) UNION ALL (
+                SELECT phrase_id, updated_by, updated_at, gloss, state
+                FROM gloss_history
+            )
+        ) log
+        JOIN phrase ON phrase.id = log.phrase_id
+        JOIN phrase_word ON phrase_word.phrase_id = phrase.id
+        JOIN word ON word.id = phrase_word.word_id
+        JOIN verse ON verse.id = word.verse_id
+        WHERE log.updated_at < (DATE_BIN('7 days', DATE_TRUNC('day', $1), TIMESTAMP '2024-12-15'))
+            AND (phrase.deleted_at IS NULL
+                OR phrase.deleted_at < (DATE_BIN('7 days', DATE_TRUNC('day', $1), TIMESTAMP '2024-12-15')))
+        ORDER BY log.phrase_id, phrase_word.word_id, verse.book_id, log.updated_at DESC
+    ) log
+    GROUP BY log.language_id, log.book_id, log.updated_by
+    ORDER BY log.language_id, log.book_id, log.updated_by
+    ON CONFLICT (language_id, book_id, user_id, week)
+    DO UPDATE SET
+        approved_count = EXCLUDED.approved_count,
+        unapproved_count = EXCLUDED.unapproved_count;
+$$;
+
 COMMIT;
