e02f5311ca1420d07f2215f6f83673ed1f5c5623
Adrian Rocke <me@adrianrocke.com>
2025-02-03 20:21:20 -0600

    ci: add postgres to ci test job

--- db/scripts/docker-initdb-test.sh
+++ db/scripts/docker-initdb-test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
-
-# Required to load pg_cron
-pg_ctl restart
-
 psql --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" -f /db/scripts/init_extensions.sql
 
--- db/scripts/init_extensions.sql
+++ db/scripts/init_extensions.sql
@@ -1,2 +1,12 @@
-CREATE EXTENSION IF NOT EXISTS pg_cron WITH SCHEMA pg_catalog;
+DO $$
+BEGIN
+    -- Check if pg_cron extension is available to install
+    IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'pg_cron') THEN
+        CREATE EXTENSION IF NOT EXISTS pg_cron WITH SCHEMA pg_catalog;
+    ELSE
+        RAISE NOTICE 'skipping pg_cron';
+    END IF;
+END;
+$$;
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
