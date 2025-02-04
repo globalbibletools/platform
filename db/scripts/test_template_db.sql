781e0be43ef121ff1e6dbc216de5ee3776dda38b
Adrian Rocke <me@adrianrocke.com>
2025-02-03 20:34:52 -0600

    test: create extensions on test startup

--- db/scripts/test_template_db.sql
+++ db/scripts/test_template_db.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS test_template;
+\i db/scripts/init_extensions.sql
 
+DROP DATABASE IF EXISTS test_template;
 CREATE DATABASE test_template;
 
 \c test_template
-
 \i db/scripts/schema.sql
