9ca4813f6bbaa4691bee7170cade947e16e6917d
Adrian Rocke <me@adrianrocke.com>
2025-02-27 19:02:19 -0600

    fix: tailwind config for modules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,6 +74,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/*/views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/*/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/languages.json",
   ],
   theme: {
