2286394469cdfb64ca1afa8999e88c0dadc75fc5
Adrian Rocke <me@adrianrocke.com>
2025-06-29 19:18:11 -0500

    refactor: pull out query for redirecting to verse

--- src/modules/translation/actions/redirectToUnapproved.ts
+++ src/modules/translation/actions/redirectToUnapproved.ts
@@ -3,9 +3,9 @@
 import * as z from "zod";
 import { getLocale, getTranslations } from "next-intl/server";
 import { parseForm } from "@/form-parser";
-import { query } from "@/db";
 import { serverActionLogger } from "@/server-action";
 import { redirect } from "next/navigation";
+import glossRepository from "../data-access/GlossRepository";
 
 const requestSchema = z.object({
   code: z.string(),
@@ -23,55 +23,17 @@ export async function redirectToUnapproved(
     return;
   }
 
-  let result = await query<{ nextUnapprovedVerseId: string }>(
-    `
-        SELECT w.verse_id as "nextUnapprovedVerseId"
-        FROM word AS w
-        LEFT JOIN LATERAL (
-          SELECT g.state AS state FROM phrase_word AS phw
-          JOIN phrase AS ph ON ph.id = phw.phrase_id
-          LEFT JOIN gloss AS g ON g.phrase_id = ph.id
-          WHERE phw.word_id = w.id
-			      AND ph.language_id = (SELECT id FROM language WHERE code = $1)
-			      AND ph.deleted_at IS NULL
-        ) AS g ON true
-        WHERE w.verse_id > $2
-          AND (g.state = 'UNAPPROVED' OR g.state IS NULL)
-        ORDER BY w.id
-        LIMIT 1
-        `,
-    [request.data.code, request.data.verseId],
+  const nextVerseId = await glossRepository.findNextUnapproved(
+    request.data.code,
+    request.data.verseId,
   );
 
-  if (result.rows.length === 0) {
-    result = await query<{ nextUnapprovedVerseId: string }>(
-      `
-            SELECT w.verse_id as "nextUnapprovedVerseId"
-            FROM word AS w
-            LEFT JOIN LATERAL (
-              SELECT g.state AS state FROM phrase_word AS phw
-              JOIN phrase AS ph ON ph.id = phw.phrase_id
-              LEFT JOIN gloss AS g ON g.phrase_id = ph.id
-              WHERE phw.word_id = w.id
-                      AND ph.language_id = (SELECT id FROM language WHERE code = $1)
-                      AND ph.deleted_at IS NULL
-            ) AS g ON true
-            WHERE (g.state = 'UNAPPROVED' OR g.state IS NULL)
-            ORDER BY w.id
-            LIMIT 1
-            `,
-      [request.data.code],
-    );
-
-    if (result.rows.length === 0) {
-      logger.error("all verses are approved");
-      const t = await getTranslations("TranslationToolbar");
-      return t("errors.all_approved");
-    }
+  if (typeof nextVerseId !== "string") {
+    logger.error("all verses are approved");
+    const t = await getTranslations("TranslationToolbar");
+    return t("errors.all_approved");
   }
 
   const locale = await getLocale();
-  redirect(
-    `/${locale}/translate/${request.data.code}/${result.rows[0].nextUnapprovedVerseId}`,
-  );
+  redirect(`/${locale}/translate/${request.data.code}/${nextVerseId}`);
 }
--- src/modules/translation/data-access/GlossRepository.ts
+++ src/modules/translation/data-access/GlossRepository.ts
@@ -69,6 +69,54 @@ const glossRepository = {
     return result.rows;
   },
 
+  async findNextUnapproved(
+    languageCode: string,
+    verseId: string,
+  ): Promise<string | undefined> {
+    let result = await query<{ nextUnapprovedVerseId: string }>(
+      `
+        SELECT w.verse_id as "nextUnapprovedVerseId"
+        FROM word AS w
+        LEFT JOIN LATERAL (
+          SELECT g.state AS state FROM phrase_word AS phw
+          JOIN phrase AS ph ON ph.id = phw.phrase_id
+          LEFT JOIN gloss AS g ON g.phrase_id = ph.id
+          WHERE phw.word_id = w.id
+			      AND ph.language_id = (SELECT id FROM language WHERE code = $1)
+			      AND ph.deleted_at IS NULL
+        ) AS g ON true
+        WHERE w.verse_id > $2
+          AND (g.state = 'UNAPPROVED' OR g.state IS NULL)
+        ORDER BY w.id
+        LIMIT 1
+        `,
+      [languageCode, verseId],
+    );
+
+    if (result.rows.length === 0) {
+      result = await query<{ nextUnapprovedVerseId: string }>(
+        `
+            SELECT w.verse_id as "nextUnapprovedVerseId"
+            FROM word AS w
+            LEFT JOIN LATERAL (
+              SELECT g.state AS state FROM phrase_word AS phw
+              JOIN phrase AS ph ON ph.id = phw.phrase_id
+              LEFT JOIN gloss AS g ON g.phrase_id = ph.id
+              WHERE phw.word_id = w.id
+                      AND ph.language_id = (SELECT id FROM language WHERE code = $1)
+                      AND ph.deleted_at IS NULL
+            ) AS g ON true
+            WHERE (g.state = 'UNAPPROVED' OR g.state IS NULL)
+            ORDER BY w.id
+            LIMIT 1
+            `,
+        [languageCode],
+      );
+    }
+
+    return result.rows[0]?.nextUnapprovedVerseId;
+  },
+
   async update(options: UpdateGlossOptions) {
     await query(
       `insert into gloss (phrase_id, state, gloss, updated_at, updated_by, source)
