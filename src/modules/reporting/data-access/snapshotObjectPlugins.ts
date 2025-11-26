5499a97a5e985d0021dd0a8c95ca8b163786bdeb
Adrian Rocke <me@adrianrocke.com>
2025-11-26 15:58:22 -0600

    convert tracking events snapshots to new format

--- src/modules/reporting/data-access/snapshotObjectPlugins.ts
+++ src/modules/reporting/data-access/snapshotObjectPlugins.ts
@@ -1,20 +1,48 @@
+import { Readable } from "stream";
+import { copyStream, query, queryStream } from "@/db";
 import {
+  PostgresTextFormatTransform,
   SnapshotObjectPlugin,
   createPostgresSnapshotObjectPlugin,
 } from "@/modules/snapshots/model";
 
 export const reportingSnapshotObjectPlugins: SnapshotObjectPlugin[] = [
-  createPostgresSnapshotObjectPlugin({
+  {
     resourceName: "tracking_event",
-    readSqlQuery: `
-        select * from tracking_event
-        where language_id = $1
-      `,
-    deleteSqlQuery: `
-        delete from tracking_event
-        where language_id = $1
-      `,
-  }),
+    async read(languageId: string): Promise<Readable> {
+      return queryStream(
+        `
+          select * from tracking_event
+          where language_id = $1
+        `,
+        [languageId],
+      );
+    },
+    async clear(languageId: string): Promise<void> {
+      await query(
+        `
+          delete from tracking_event
+          where language_id = $1
+        `,
+        [languageId],
+      );
+    },
+    async write(stream: Readable): Promise<void> {
+      await copyStream(
+        "tracking_event",
+        stream.pipe(
+          new PostgresTextFormatTransform([
+            (event) => event.id,
+            (event) => event.type,
+            (event) => JSON.stringify(event.data),
+            (event) => event.user_id,
+            (event) => event.language_id,
+            (event) => event.created_at,
+          ]),
+        ),
+      );
+    },
+  },
   createPostgresSnapshotObjectPlugin({
     resourceName: "weekly_contribution_statistics",
     readSqlQuery: `
