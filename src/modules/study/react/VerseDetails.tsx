702ded38611ae367243405cdb72f0f753d2ec876
Adrian Rocke <me@adrianrocke.com>
2025-12-28 21:09:08 -0600

    feat: show greek questions and commentary in sidebar

--- src/modules/study/react/VerseDetails.tsx
+++ src/modules/study/react/VerseDetails.tsx
@@ -31,7 +31,7 @@ export default function VerseDetails({
         <Tab.Group selectedIndex={tabIndex} onChange={setTabIndex}>
           <Tab.List className="flex flex-row items-end">
             <div className="border-b border-blue-800 dark:border-green-400 h-full w-2"></div>
-            {[t("tabs.questions")].map((title) => (
+            {[t("tabs.questions"), "Commentary"].map((title) => (
               <Fragment key={title}>
                 <Tab
                   className="
@@ -46,7 +46,51 @@ export default function VerseDetails({
             ))}
             <div className="border-b border-blue-800 dark:border-green-400 h-full grow"></div>
           </Tab.List>
-          <Tab.Panels className="overflow-y-auto grow px-4 pt-4 mb-4"></Tab.Panels>
+          <Tab.Panels className="overflow-y-auto grow px-4 pt-4 mb-4">
+            <Tab.Panel>
+              <ol className="px-4">
+                <li className="font-mixed list-decimal mb-3">
+                  <div className="mb-1">Τίς ἐστιν ὁ Παῦλος;</div>
+                  <div className="text-sm ms-4">
+                    Ὁ Παῦλός ἐστιν ὁ δέσµιος τοῦ Χριστοῦ Ἰησοῦ.
+                  </div>
+                </li>
+                <li className="font-mixed list-decimal mb-3">
+                  <div className="mb-1">Τίνος δέσµιός ἐστιν ὁ Παῦλος;</div>
+                  <div className="text-sm ms-4">
+                    Τοῦ Χριστοῦ Ἰησοῦ δέσµιός ἐστιν ὁ Παῦλος.
+                  </div>
+                </li>
+                <li className="font-mixed list-decimal mb-3">
+                  <div className="mb-1">
+                    Μετὰ τίνος ὁ Παῦλος γράφει τὴν ἐπιστολὴν;
+                  </div>
+                  <div className="text-sm ms-4">
+                    Μετὰ τοῦ Τιµοθέου γράφει ὁ Παῦλος τὴν ἐπιστολήν.
+                  </div>
+                </li>
+              </ol>
+            </Tab.Panel>
+            <Tab.Panel>
+              <p className="font-mixed">
+                Ὁ µὲν Παῦλος ταὺτην τὴν ἐπιστολὴν ἔγραψε σὺν τῷ Τιµοθέῳ, αὐτὸς
+                δὲ δέσµιος τοῦ Χριστοῦ Ἰησοῦ ἦν· νῦν δὲ ἔγραψε ταύτην τὴν
+                ἐπιστολὴν ἐν τῇ φυλακῇ. Ὁ γὰρ δέσµιος ἦν ὑπὲρ τοῦ εὐαγγελίου τοῦ
+                Χριστοῦ, οὐχ ὡς δέσµιος ἀνθρώπων µένων ἐν τῇ φυλακῇ τῶν Ῥωµαίων,
+                ἀλλὰ ὡς τοῦ Χριστοῦ Ἰησοῦ. Δέδεται γὰρ διὰ τὸ κηρύσσειν τὸ
+                εὐαγγέλιον τοῦ Χριστοῦ καὶ τὸ µαρτυρεῖν περὶ τῆς ζωῆς καὶ ἔργου
+                τοῦ Ἰησοῦ Χριστοῦ· Ὁ δὲ Τιµόθεος ὁ ἀδελφὸς καὶ ὁ γράφων σὺν
+                Παύλῳ· εἰργάζετο γὰρ ὥς κοινωνός τε καὶ συνεργὸς ἐν τῷ ἔργῳ τοῦ
+                Χριστοῦ εὐαγγελίου. Ὁ δέ Φιλήµων ἐστιν ὁ δεχόµενος τὴν
+                ἐπιστολήν. Ἡ γὰρ ἐπιστολὴ πρὸς αὐτὸν τὸν ἀγαπητὸν καὶ συνεργὸν
+                ἐν Χριστῷ ἀπεστάλην ὑπὸ τοῦ Παύλου καὶ Τιµοθέου. Αὐτὸς δὲ
+                καλεῖται ἀγαπητός, ὅτι λίαν ἀγαπᾶται ὑπὸ Παύλου καὶ Τιµοθέου·
+                καὶ συνεργὸς ὅτι εἰργάζετο ἐν τοῖς ἔργοις τοῦ κυρίου σὺν ἄλλοις
+                πιστοῖς ἐν Χριστῷ, µάλιστα δὲ ἐν τῇ κατ’ οἶκον αὐτοῦ ἐκκλησίᾳ ἐν
+                ταῖς Κολοσσαῖς.{" "}
+              </p>
+            </Tab.Panel>
+          </Tab.Panels>
         </Tab.Group>
       </div>
     </div>
