6e08d8954f75ac777c63c6441f41663fd3ef79cb
Adrian Rocke <me@adrianrocke.com>
2026-01-04 14:42:33 -0600

    fix: use case import in user client

--- src/modules/users/public/UserClient.ts
+++ src/modules/users/public/UserClient.ts
@@ -1,16 +1,13 @@
 import userQueryService from "../data-access/UserQueryService";
-import userRepository from "../data-access/userRepository";
-import InviteUser from "../use-cases/InviteUser";
+import { inviteUser } from "../use-cases/inviteUser";
 import { PublicUserView } from "./types";
-
-const inviteUserUseCase = new InviteUser(userRepository);
 
 export const userClient = {
   async findOrInviteUser(email: string): Promise<string> {
     let user = await userQueryService.findByEmail(email);
     if (user) return user.id;
 
-    const { userId } = await inviteUserUseCase.execute({ email });
+    const { userId } = await inviteUser({ email });
     return userId;
   },
 
