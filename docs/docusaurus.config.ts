2d3f7f786642ca7910797695cb718607a5d88726
Adrian Rocke <me@adrianrocke.com>
2026-01-17 07:33:02 -0600

    docs: change docs site hostname

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://globalbibletools.github.io.com",
+  url: "https://developers.globalbibletools.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/platform/",
