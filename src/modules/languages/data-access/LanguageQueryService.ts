b01e9208c34361e52b24651712588588817b7278
Adrian Rocke <me@adrianrocke.com>
2026-01-25 20:47:58 -0600

    refactor: extract language settings read model (#154)

--- src/modules/languages/data-access/LanguageQueryService.ts
+++ src/modules/languages/data-access/LanguageQueryService.ts
@@ -19,37 +19,7 @@ type LanguageProgressQueryResult = {
   nextVerse: string | null;
 };
 
-export type LanguageSettingsQueryResult = Pick<
-  DbLanguage,
-  | "englishName"
-  | "localName"
-  | "code"
-  | "font"
-  | "textDirection"
-  | "translationIds"
-  | "referenceLanguageId"
->;
-
 export const languageQueryService = {
-  async findSettingsByCode(
-    code: string,
-  ): Promise<LanguageSettingsQueryResult | undefined> {
-    const result = await query<LanguageSettingsQueryResult>(
-      `
-        select
-          english_name as "englishName", local_name as "localName", code, font,
-          text_direction as "textDirection",
-          translation_ids as "translationIds",
-          reference_language_id as "referenceLanguageId"
-        from language
-        where code = $1
-      `,
-      [code],
-    );
-
-    return result.rows[0];
-  },
-
   async findProgressByCode(
     code: string,
   ): Promise<LanguageProgressQueryResult[]> {
