33c0ab7daad9742a42a5fce2e973dde666af70f2
Adrian Rocke <me@adrianrocke.com>
2026-02-02 19:07:42 -0600

    chore: upgrade tailwind to v4 and retool config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
-import defaultTheme from "tailwindcss/defaultTheme";
-import colors from "tailwindcss/colors";
 
 // Our tailwind rtl utilities are originally based off of tailwindcss-rtl.
 const rtl = plugin(({ addUtilities, matchUtilities, theme }) => {
@@ -70,81 +68,6 @@ const rtl = plugin(({ addUtilities, matchUtilities, theme }) => {
 });
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./src/languages.json"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
-        hebrew: [
-          "var(--font-sbl-hebrew)",
-          "var(--font-sans)",
-          ...defaultTheme.fontFamily.sans,
-        ],
-        greek: [
-          "var(--font-sbl-greek)",
-          "var(--font-sans)",
-          ...defaultTheme.fontFamily.sans,
-        ],
-        mixed: [
-          "var(--font-sbl-greek)",
-          "var(--font-sbl-hebrew)",
-          "var(--font-sans)",
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
-      boxShadow: {
-        left: "-1px 0 3px 0 rgb(0 0 0 / 0.1), -1px 0 2px -1px rgb(0 0 0 / 0.1)",
-        right: "1px 0 3px 0 rgb(0 0 0 / 0.1), 1px 0 2px -1px rgb(0 0 0 / 0.1)",
-      },
-    },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: colors.stone,
-      red: colors.red,
-      green: {
-        50: "#F3FAF8",
-        100: "#D8EFEB",
-        200: "#B2DDD7",
-        300: "#83C5BE", // brand
-        400: "#59A8A2",
-        500: "#3F8D88",
-        600: "#31706E",
-        700: "#2A5B5A",
-        800: "#254A49",
-        900: "#233E3E",
-        950: "#0C0A09",
-      },
-      blue: {
-        50: "#F0FFFD",
-        100: "#C7FFFB",
-        200: "#8AFEF9",
-        300: "#55F6EE",
-        400: "#13ECE8",
-        500: "#01D0D0",
-        600: "#01A5A7",
-        700: "#018084",
-        800: "#066F74", // brand
-        900: "#0A5457",
-        950: "#002E33",
-      },
-      brown: {
-        50: "#F8F7EE",
-        100: "#F0EDD9", // brand
-        200: "#DED6AA",
-        300: "#CBBA79",
-        400: "#BBA154",
-        500: "#AC8E46",
-        600: "#93733B",
-        700: "#775731",
-        800: "#64492F",
-        900: "#573E2C",
-        950: "#312117",
-      },
-    },
-  },
   plugins: [
     rtl,
     require("@headlessui/tailwindcss"),
