c66be185b4406887a6e68339fd0689f14237e860
Adrian Rocke <me@adrianrocke.com>
2026-02-02 23:21:27 -0600

    chore: upgrade and configure next-intl v4

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import createNextIntlPlugin from "next-intl/plugin";
 import { merge } from "webpack-merge";
 
-const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
---
+++ src/i18n/routing.ts
@@ -0,0 +1,7 @@
+import { defineRouting } from "next-intl/routing";
+import languages from "../languages.json" assert { type: "json" };
+
+export const routing = defineRouting({
+  locales: Object.keys(languages),
+  defaultLocale: "en",
+});
