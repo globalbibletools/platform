f69e2b9e5534d4d1ba2b46e16aeb13488f6c1728
Adrian Rocke <me@adrianrocke.com>
2026-02-03 13:55:13 -0600

    fix: stop using es module for next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
+import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import { merge } from "webpack-merge";
 
 const withNextIntl = createNextIntlPlugin("./src/shared/i18n/request.ts");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   webpack: (config, { isServer, nextRuntime }) => {
     if (isServer && nextRuntime === "nodejs") {
       return merge(config, {
