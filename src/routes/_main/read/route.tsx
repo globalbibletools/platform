3ec5f4be4dd5ae02e9d6d8b0e398fdc4dadac220
Adrian Rocke <me@adrianrocke.com>
2026-03-31 08:40:52 -0500

    feat: remember where you last read or translated

--- src/routes/_main/read/route.tsx
+++ src/routes/_main/read/route.tsx
@@ -1,15 +1,21 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { readReadNavigationCookie } from "@/shared/navigationCookies";
 
 export const Route = createFileRoute("/_main/read")({
   beforeLoad: ({ location }) => {
     const pathParts = location.pathname.split("/");
-    if (pathParts.length === 4) return;
+    if (pathParts.length === 4) {
+      return;
+    }
 
-    // TODO: save last visited cookie and return when redirecting
-    const [, , code = "eng", chapterId = "01001"] = pathParts;
+    const cookieValue = readReadNavigationCookie();
+
     throw redirect({
       to: "/read/$code/$chapterId",
-      params: { code, chapterId },
+      params: {
+        code: cookieValue?.code ?? "eng",
+        chapterId: cookieValue?.chapterId ?? "01001",
+      },
     });
   },
 });
