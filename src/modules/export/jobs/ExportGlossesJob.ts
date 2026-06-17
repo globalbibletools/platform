cd936a224e9b2b8d64dba52a6fe304d662cbb1fe
Adrian Rocke <me@adrianrocke.com>
2026-06-16 22:21:44 -0500

    export: accept null job payload on export glosses job

--- src/modules/export/jobs/ExportGlossesJob.ts
+++ src/modules/export/jobs/ExportGlossesJob.ts
@@ -1,9 +1,11 @@
 import { createJobModel } from "@/shared/jobs/model";
 import * as z from "zod";
 
-const ExportGlossesPayloadSchema = z.object({
-  windowDays: z.number().optional(),
-});
+const ExportGlossesPayloadSchema = z
+  .object({
+    windowDays: z.number().optional(),
+  })
+  .nullish();
 
 export class ExportGlossesJob extends createJobModel({
   type: "export_glosses",
