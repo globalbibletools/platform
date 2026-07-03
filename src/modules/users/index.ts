9bcb4cecbbbf31a26901c6d1aa6e008013dc93c0
Adrian Rocke <me@adrianrocke.com>
2026-07-03 10:06:18 -0500

    remove old language invite use case

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -1,11 +1,6 @@
 export { SystemRoleRaw } from "./model/SystemRole";
 
 export {
-  inviteUser,
-  type InviteUserRequest,
-  type InviteUserResponse,
-} from "./use-cases/inviteUser";
-export {
   reinviteUser,
   type ReinviteUserRequest,
 } from "./use-cases/reinviteUser";
